c9dda7749f743d0832ad689535483547b4bcb59f
Nihal mohamed <73734438+Nihalmohamed-10@users.noreply.github.com>
2025-01-31 18:32:36 +0530

    services changes

--- src/containers/services/ServiceHero.jsx
+++ src/containers/services/ServiceHero.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import ServiceHero from '../../assets/images/serviceHero.png'; 
+import ServicesHero from '../../assets/images/servicesHero.png'; 
 
 function ServiceHero() {
   return (
     <div className='relative'>
         <div className='w-[100%] h-[284px]'>
-            <img className='w-[100%] h-[184px]' src={ServiceHero} alt="Service Hero" />
+            <img className='w-[100%] h-[184px]' src={ServicesHero} alt="Service Hero" />
         </div>
         <h2 className='text-[22px] font-bold text-[#FFFFFF] absolute left-[50px] bottom-[138px] xl:text-[30px]'>Services</h2> 
     </div>
